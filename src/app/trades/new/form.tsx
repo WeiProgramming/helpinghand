123a9bc021207d9441e5d2e712a79215d04daf68
WeiProgramming <wei.leung1995@gmail.com>
2026-04-21 13:12:35 -0700

    simple trade form styling

--- src/app/trades/new/form.tsx
+++ src/app/trades/new/form.tsx
@@ -42,33 +42,45 @@ export default function TradeForm() {
     }
 
     return (
-        <form className="flex flex-col gap-6">
-            <input className="border-2 border-gray-500" name="ticker" placeholder="Ticker" onChange={e => {
+        <form className="flex flex-col gap-6 border-4 border-gray-500 p-6">
+            <label className="flex flex-col">
+                    Ticker
+            <input className="rounded-md py-2 px-3 mt-2 border-2 border-gray-500" name="ticker" placeholder="Ticker" onChange={e => {
                 setTradeDetail((prev) => {
                     return {...prev, ticker: e.target.value}
                 })
             }}/>
-            <div className="flex flex-row gap-1">
-                <input className="border-2 border-gray-500" type="number" name="entry" placeholder="Entry" onChange={e => {
-                setTradeDetail((prev) => {
-                    return {...prev, entry: Number(e.target.value)}
-                })
-            }}/>
-                <input className="border-2 border-gray-500" name="exit" placeholder="Exit" onChange={e => {
-                setTradeDetail((prev) => {
-                    return {...prev, exit: Number(e.target.value)}
-                })
-            }}/>
-                <input className="border-2 border-gray-500" name="stop" placeholder="Stop" onChange={e => {
-                setTradeDetail((prev) => {
-                    return {...prev, stop: Number(e.target.value)}
-                })
-            }}/>
+            </label>
+            <div className="flex flex-row gap-1 justify-between sm:flex-col">
+                <label className="flex flex-col mt-2">
+                    Entry
+                    <input className="rounded-md py-2 px-3 mt-2 border-2 border-gray-500" type="number" name="entry" placeholder="Entry" onChange={e => {
+                        setTradeDetail((prev) => {
+                            return { ...prev, entry: Number(e.target.value) }
+                        })
+                    }} />
+                </label>
+                <label className="flex flex-col mt-2">
+                    Exit
+                    <input className="rounded-md py-2 px-3 mt-2 border-2 border-gray-500" name="exit" placeholder="Exit" onChange={e => {
+                        setTradeDetail((prev) => {
+                            return { ...prev, exit: Number(e.target.value) }
+                        })
+                    }} />
+                </label>
+                <label className="flex flex-col mt-2">
+                    Stop
+                    <input className="rounded-md py-2 px-3 mt-2 border-2 border-gray-500" name="stop" placeholder="Stop" onChange={e => {
+                        setTradeDetail((prev) => {
+                            return { ...prev, stop: Number(e.target.value) }
+                        })
+                    }} />
+                </label>
             </div>
             <div className="flex flex-col gap-4">
                 <label>
                     Setup:
-                    <select name="setup" onChange={e => {
+                    <select className="block w-full px-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body" name="setup" onChange={e => {
                 setTradeDetail((prev) => {
                     return {...prev, setup: e.target.value}
                 })
@@ -79,7 +91,7 @@ export default function TradeForm() {
                 </label>
                 <label>
                     Direction:
-                    <select name="Direction"  onChange={e => {
+                    <select className="block w-full px-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body" name="Direction"  onChange={e => {
                 setTradeDetail((prev) => {
                     return {...prev, direction: e.target.value}
                 })
@@ -91,7 +103,7 @@ export default function TradeForm() {
                 </label>
                 <label>
                     Timeframe:
-                    <select name="timeframe"  onChange={e => {
+                    <select className="block w-full px-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body" name="timeframe"  onChange={e => {
                 setTradeDetail((prev) => {
                     return {...prev, timeframe: e.target.value}
                 })
@@ -106,7 +118,7 @@ export default function TradeForm() {
                 </label>
                 <label>
                     Emotion:
-                    <select name="emotion"  onChange={e => {
+                    <select className="block w-full px-3 py-2.5  border border-default-medium text-heading text-sm rounded-base shadow-xs" name="emotion"  onChange={e => {
                 setTradeDetail((prev) => {
                     return {...prev, emotion: e.target.value}
                 })
@@ -117,11 +129,14 @@ export default function TradeForm() {
                     </select>
                 </label>
             </div>
-            <textarea className="border-2 border-gray-500" name="notes" placeholder="Notes"  onChange={e => {
+            <label className="flex flex-col">
+                    Notes
+            <textarea className="rounded-md p-2 mt-2 border-2 border-gray-500" name="notes" placeholder="Notes"  onChange={e => {
                 setTradeDetail((prev) => {
                     return {...prev, notes: e.target.value}
                 })
             }}/>
+            </label>
             <div className="flex flex-row gap-6">
                 <button className="bg-blue-600 w-2/4 rounded-xs" onClick={e => {e.preventDefault(); newTrade()}}>Submit</button>
                 <button className="bg-gray-500 w-2/4 rounded-xs" onClick={(e) => {clearForm()}}>Clear</button>
--- src/app/trades/new/page.tsx
+++ src/app/trades/new/page.tsx
@@ -2,8 +2,8 @@ import TradeForm from "./form";
 
 export default function New() {
     return (
-        <div className="w-4/6">
-            <h2>Enter your trade details</h2>
+        <div className="w-4/6 p-4 ">
+            <h2 className="mb-4">Add Trade</h2>
             <TradeForm/>
         </div>
     )
